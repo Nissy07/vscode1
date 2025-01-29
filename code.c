8905102609dd82a06e32c33632c17fe8f55f397a
kyosei <r202304944rk@jindai.jp>
2025-01-29 19:46:59 +0900

    loop using for

--- code.c
+++ code.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void){
-    int i = 10;
-    printf("hello %d", i);
-    return 0;
+int main(void) {
+  int i = 10;
+  for (i = 0; i < 10; i++);
+  printf("hello %d", i);
+  return 0;
 }
